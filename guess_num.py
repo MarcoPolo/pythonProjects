cd311645169f0560ddcc7c982f2c48670d7b4219
Marco Munizaga <Marco.emunizaga@gmail.com>
2011-02-17 19:28:53 -0500

    changed num_guess removed while loop

--- guess_num.py
+++ guess_num.py
@@ -13,22 +13,21 @@ def findMid(pt1,pt2):
  return midpt
 
 def findNum(firstNum,endNum):
- while(isnum):
-   numGuess=findMid(firstNum, endNum)
-   print 'Is ' + str(numGuess) + ' too high (h) or too low (l) or correct (c)'
-   usr = raw_input()
-   if(usr=='h'):
-    endNum=numGuess
-    findNum(firstNum, endNum)
-    return
-   elif usr=='l':
-    firstNum=numGuess
-    findNum(firstNum, endNum)
-   elif usr=='c':
-    print "Sw33t"
-   else:
-    print "Sorry I didn't catch that"
-    findNum(firstNum, endNum)
+  numGuess=findMid(firstNum, endNum)
+  print 'Is ' + str(numGuess) + ' too high (h) or too low (l) or correct (c)'
+  usr = raw_input()
+  if(usr=='h'):
+   endNum=numGuess
+   findNum(firstNum, endNum)
+   return
+  elif usr=='l':
+   firstNum=numGuess
+   findNum(firstNum, endNum)
+  elif usr=='c':
+   print "Sw33t"
+  else:
+   print "Sorry I didn't catch that"
+   findNum(firstNum, endNum)
 
 if __name__=='__main__':
 	main()
